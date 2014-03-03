0d8ff3be3539236e60c6590c9c45b705974dcae8
Stefan <stefan@cyphoria.net>
2014-03-03 12:07:47 +0100

    sauce labs unterstützung hinzugefügt

--- test/acceptance/infrastruktur/TestUmgebung.scala
+++ test/acceptance/infrastruktur/TestUmgebung.scala
@@ -1,18 +1,19 @@
 package acceptance.infrastruktur
 
-import play.api.test.{Helpers, FakeApplication, TestServer, TestBrowser}
+import play.api.test.{Helpers, TestBrowser}
 import play.api.test.Helpers._
 import play.api.test.TestServer
 import play.api.test.FakeApplication
 import cucumber.api.scala.ScalaDsl
-import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.remote.DesiredCapabilities
 import com.saucelabs.selenium.client.factory.SeleniumFactory
+import scala.util.Properties
 
 
 trait Browser {
 
   def browser = TestUmgebung.browser.get
+
   //def startSeite = browser.createPage(classOf[StartSeite])
 
 }
@@ -20,42 +21,51 @@ trait Browser {
 
 object TestUmgebung extends ScalaDsl {
 
-  val anwendung = new FakeAnwendungInitialisierer().initAnwendung
+  val anwendung = new TestAnwendungInitialisierer().initAnwendung
   var browser: Option[TestBrowser] = None
 
 
-  Before { _ => Unit
-    browser = Some(new LocalFirefoxInitialisierer().initBrowser)
+  Before {
+    _ => Unit
+      browser = Some(new FirefoxInitialisierer().initBrowser)
   }
 
-  After { _ => Unit
-    browser.foreach(_.quit)
-    browser = None
+  After {
+    _ => Unit
+      browser.foreach(_.quit)
+      browser = None
   }
 }
 
 case class Anwendung(url: String)
 
-class LocalFirefoxInitialisierer {
-  val webDriver: Class[FirefoxDriver] = Helpers.FIREFOX
+class FirefoxInitialisierer {
 
   def initBrowser = new TestBrowser(
     SeleniumFactory.createWebDriver(DesiredCapabilities.firefox()),
     Some(TestUmgebung.anwendung.url))
 }
 
-class FakeAnwendungInitialisierer {
-  val applicationPort: Int = Helpers.testServerPort
+class TestAnwendungInitialisierer {
+  private val applicationPort: Int = Helpers.testServerPort
 
   def initAnwendung = {
+    val startUrl = Properties.envOrNone("SELENIUM_URL")
+
+    new Anwendung(startUrl.getOrElse(initLokal()))
+  }
+
+  private def initLokal(): String = {
     val application = FakeApplication(additionalConfiguration = inMemoryDatabase())
     val server = TestServer(applicationPort, application)
     server.start()
 
-    Runtime.getRuntime.addShutdownHook(new Thread(){
+    Runtime.getRuntime.addShutdownHook(new Thread() {
       override def run() = server.stop()
     })
 
-    new Anwendung("http://localhost:" + applicationPort)
+    "http://localhost:" + applicationPort
+
   }
+
 }
