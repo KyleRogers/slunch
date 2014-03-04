ee9ef3a7a4e9ba91cce02158dceada9c5859af70
Stefan <stefan@cyphoria.net>
2014-03-04 20:16:09 +0100

    fixed cucumber json path/file name (may not clash with html stuff)

--- test/acceptance/RunAllAcceptanceTests.java
+++ test/acceptance/RunAllAcceptanceTests.java
@@ -8,6 +8,6 @@ import org.junit.runner.RunWith;
  * @author Stefan Penndorf <stefan@cyphoria.net>
  */
 @RunWith(Cucumber.class)
-@CucumberOptions(format = {"pretty", "html:target/cucumber", "json:target/cucumber"}, features = "test/resources/features", monochrome = false)
+@CucumberOptions(format = {"pretty", "html:target/cucumber", "json:target/cucumber.json"}, features = "test/resources/features", monochrome = false)
 public class RunAllAcceptanceTests {
 }
